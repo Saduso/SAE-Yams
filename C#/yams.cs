b366749f05b297657ee2600402ff32256b613f6b
Liam <liam.guilleux-riff@etu.unistra.fr>
2024-11-05 11:48:41 +0100

    Ajout de LancerDes

--- C#/yams.cs
+++ C#/yams.cs
@@ -1,6 +1,17 @@
+using System;
 public class yams
 {
     public static void Main () {
         
+    }
+    /*
+        Genere un nombre aleatoire entre 1 et 6 compris
+    */
+    public static int LancerDes(){
+        Random rnd = new Random();
+        int randInt = rnd.Next(1,7);
+
+        return randInt;
+
     }
 }
