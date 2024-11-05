585e73261d14669ada70b59f698a36302e7ccac3
Guillaume <guillaume.simon4@etu.unistra.fr>
2024-11-05 17:18:47 +0100

    Sync

--- C#/yams.cs
+++ C#/yams.cs
@@ -1,5 +1,4 @@
 using System;
-
 public class yams
 {
     public static void Main () {
@@ -14,45 +13,6 @@ public class yams
         int randInt = rnd.Next(1,7);
 
         return randInt;
-    }
 
-    public struct Dé
-    {
-        // Structure d'un dé.
-        private int _val;
-        private bool _garder;
-
-		public Dé(int val = 0) {
-			_val = val;
-			_garder = false;
-		}
-
-        /*
-         * Val:
-         * get: Récupère la valeure du dé.
-         * set: La valeure du dé doit être compri entre 1 et 6.
-         */
-        public int Val
-        {
-            get => _val;
-            set => _val = value != 0 ? value % 7 : 1;
-        }
-
-        /*
-         * Val:
-         * get: Récupère si le dé est gardé par l'utilisateur.
-         * set: Modifie la valeur du dé.
-         */
-        public bool Garder
-        {
-            get => _garder;
-            set => _garder = value;
-        }
-
-		public void LancerDé(){
-        	Random rnd = new Random();
-        	int randInt = rnd.Next(1,7);
-        	Val = randInt;
-		}
     }
 }
