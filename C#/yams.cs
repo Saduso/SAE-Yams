ebb1bb75f97ccb48e3e715afc9a0d479aebc7926
Guillaume <guillaume.simon4@etu.unistra.fr>
2024-11-05 11:54:58 +0100

    Ajout de la structure Dé, val, garder

--- C#/yams.cs
+++ C#/yams.cs
@@ -3,4 +3,38 @@ public class yams
     public static void Main () {
         
     }
+    /*
+        Genere un nombre aleatoire entre 1 et 6 compris
+    */
+    public static int LancerDes(){
+        Random rnd = new Random();
+        int randInt = rnd.Next(1,7);
+
+        return randInt;
+
+    }
+
+    struct Dé
+    {
+        private int _dé;
+        private int _val;
+        private bool _garder;
+
+        public int Dé
+        {
+            get => _dé;
+            set => value % 5;
+        }
+        public int Val
+        {
+            get => _val;
+            set => value % 6;
+        }
+
+        public bool Garder
+        {
+            get => _garder;
+            set => value || _garder;
+        }
+    }
 }
